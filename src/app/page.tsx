491c979be3a9245dc6c72954c08bca9bd5e4dab4
Woojin Oh <woh@crateandbarrel.com>
2023-05-19 00:53:22 -0500

    build form

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,5 +1,48 @@
-import Image from 'next/image';
+'use client';
+
+import { useState } from 'react';
 
 export default function Home() {
-    return <main></main>;
+    const [passageInput, setPassageInput] = useState('');
+    const [questionInput, setQuestionInput] = useState('');
+
+    async function handleSubmit(e: { preventDefault: () => void }) {
+        e.preventDefault();
+        try {
+            // const response = await fetch('');
+        } catch (err) {}
+    }
+
+    return (
+        <main>
+            <div className="flex flex-col items-center my-12">
+                <h1>BibleGPT</h1>
+                <form onSubmit={handleSubmit} className="text-center">
+                    <input
+                        required
+                        type="text"
+                        name="passage"
+                        placeholder="Enter a Passage"
+                        value={passageInput}
+                        className="text-black block"
+                        onChange={(e) => {
+                            setPassageInput(e.target.value);
+                        }}
+                    />
+                    <input
+                        required
+                        type="text"
+                        name="question"
+                        placeholder="Enter a question"
+                        value={questionInput}
+                        className="text-black block"
+                        onChange={(e) => {
+                            setQuestionInput(e.target.value);
+                        }}
+                    />
+                    <input type="submit" value="Submit" />
+                </form>
+            </div>
+        </main>
+    );
 }
