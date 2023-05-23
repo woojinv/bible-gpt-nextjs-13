1894f9dfaa2169c78047e3ffc64d0d1efe293174
Woojin Oh <woh@crateandbarrel.com>
2023-05-23 00:04:34 -0500

    incorporate streaming and edge functions

--- src/app/api/route.ts
+++ src/app/api/route.ts
@@ -1,20 +1,40 @@
 import { NextRequest, NextResponse } from 'next/server';
-import { Configuration, OpenAIApi } from 'openai';
+import { OpenAIStream, OpenAIStreamPayload, chatGPTMessage } from '@/utils/OpenAIStream';
 
-const configuration = new Configuration({
-    apiKey: process.env.OPENAI_API_KEY,
-});
+if (!process.env.OPENAI_API_KEY) {
+    throw new Error('Missing env var from OpenAI');
+}
 
-const openai = new OpenAIApi(configuration);
+export const config = {
+    runtime: 'edge',
+};
 
-export async function POST(req: NextRequest) {
-    if (!configuration.apiKey) {
-        return NextResponse.json({
-            error: {
-                message: 'OpenAI API key not configured.',
-            },
-        });
-    }
-    console.log(req.body, '<<< req.body');
-    console.log('hit');
+export async function POST(req: NextRequest): Promise<NextResponse> {
+    const { passage, question } = await req.json();
+    console.log(passage, '<<< passage');
+    console.log(question, '<<< question');
+
+    const messages: chatGPTMessage[] = [
+        {
+            role: 'system',
+            content:
+                'You are a bible study assistant. You will be given a reference to a passage in the bible, as well as a question to answer regarding that passage. You will provide a succint answer to the question. Your answer will be hermeneutic. You will provide the bible reference from which you devised your answer.',
+        },
+        {
+            role: 'user',
+            content: `
+        passage: ${passage}
+        question: ${question}
+        `,
+        },
+    ];
+
+    const payload: OpenAIStreamPayload = {
+        model: 'gpt-3.5-turbo',
+        messages,
+        stream: true,
+    };
+
+    const stream = await OpenAIStream(payload);
+    return new NextResponse(stream);
 }
---
+++ src/utils/OpenAIStream.ts
@@ -0,0 +1,74 @@
+import { createParser, ParsedEvent, ReconnectInterval } from 'eventsource-parser';
+import { Configuration, OpenAIApi } from 'openai';
+
+export type ChatGPTAgent = 'user' | 'system';
+
+export interface chatGPTMessage {
+    role: ChatGPTAgent;
+    content: string;
+}
+
+export interface OpenAIStreamPayload {
+    model: string;
+    messages: chatGPTMessage[];
+    stream: boolean;
+}
+
+const configuration = new Configuration({
+    apiKey: process.env.OPENAI_API_KEY,
+});
+
+const openai = new OpenAIApi(configuration);
+
+export async function OpenAIStream(payload: OpenAIStreamPayload) {
+    const encoder = new TextEncoder();
+    const decoder = new TextDecoder();
+
+    let counter = 0;
+
+    const completion = await openai.createChatCompletion(payload);
+    // console.log(completion, '<<< completion');
+
+    const stream = new ReadableStream({
+        async start(controller) {
+            function onParse(event: ParsedEvent | ReconnectInterval) {
+                if (event.type === 'event') {
+                    const data = event.data;
+                    if (data === '[DONE]') {
+                        controller.close();
+                        return;
+                    }
+                    try {
+                        const json = JSON.parse(data);
+                        console.log(json, '<<< json');
+                        const text = json.choices[0].delta?.content || '';
+
+                        if (counter < 2 && (text.match(/\n/) || []).length) {
+                            // this is a prefix character (i.e., "\n\n"), do nothing
+                            return;
+                        }
+                        console.log(text, '<<< text');
+                        const queue = encoder.encode(text);
+                        controller.enqueue(queue);
+                        counter++;
+                    } catch (err) {
+                        controller.error(err);
+                    }
+                }
+            }
+
+            // stream response (SSE) from OpenAI may be fragmented into multiple chunks
+            // this ensures we properly read chunks and invoke an event for each SSE event stream
+            const parser = createParser(onParse);
+            // https://web.dev/streams/#asynchronous-iteration
+            console.log(completion.data, '<<< completion.data');
+            for await (const chunk of completion.data as any) {
+                console.log(chunk, '<<< chunk');
+                parser.feed(decoder.decode(chunk));
+            }
+        },
+    });
+    console.log(stream, '<<< stream');
+
+    return stream;
+}
