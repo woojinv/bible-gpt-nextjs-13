bcc14a9ed8cf659e387466ab49ef2ed2621c583e
Woojin Oh <woh@crateandbarrel.com>
2023-06-16 22:54:47 +1200

    remove saving inputs to DB

--- src/app/api/route.ts
+++ src/app/api/route.ts
@@ -2,8 +2,6 @@ import { NextRequest, NextResponse } from 'next/server';
 
 import { OpenAIStream } from '@/utils/OpenAIStream';
 
-import { sql } from '@vercel/postgres';
-
 if (!process.env.OPENAI_API_KEY) {
     throw new Error('Missing env var from OpenAI');
 }
@@ -26,9 +24,6 @@ export interface OpenAIStreamPayload {
 
 export async function POST(req: NextRequest): Promise<NextResponse> {
     const { passage, question } = await req.json();
-
-    await sql`INSERT INTO form_data (passage, question)
-    VALUES (${passage}, ${question});`;
 
     const messages: chatGPTMessage[] = [
         {
