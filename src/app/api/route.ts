2703dce96fbf8e36d1f3a44c3d5fe7d54baf648f
woojinv <oh.woojin95@gmail.com>
2023-07-26 21:52:38 -0500

    start using events in stream to save to db

--- src/app/api/route.ts
+++ src/app/api/route.ts
@@ -2,6 +2,8 @@ import { NextRequest } from 'next/server';
 import { OpenAIStream, StreamingTextResponse } from 'ai';
 import { Configuration, OpenAIApi } from 'openai-edge';
 
+import { sql } from '@vercel/postgres';
+
 const config = new Configuration({
     apiKey: process.env.OPENAI_API_KEY,
 });
@@ -41,6 +43,33 @@ export async function POST(req: NextRequest): Promise<StreamingTextResponse> {
         stream: true,
         messages,
     });
-    const stream = OpenAIStream(response);
+
+    const stream = OpenAIStream(response, {
+        onStart: async () => {
+            console.log('start stream');
+            await savePromptToDB(passage, question);
+        },
+        onCompletion: async (completion: string) => {
+            console.log('stop stream');
+            await saveCompletionToDatabase(completion);
+        },
+    });
+
     return new StreamingTextResponse(stream);
 }
+
+const savePromptToDB = async (passage: string, question: string) => {
+    console.log(passage, '<<< passage');
+    console.log(question, '<<< question');
+    // const interactionId = await sql`
+    //     INSERT INTO interactions (passage, question)
+    //     VALUES (${passage}, ${question})
+    //     RETURNING id;
+    //     `;
+
+    // console.log(interactionId, '<<< interactionId');
+};
+
+const saveCompletionToDatabase = async (completion: string) => {
+    console.log(completion, '<<< completion');
+};
