105bc09afb88a000718cb747b3feb43bae93af24
woojinv <oh.woojin95@gmail.com>
2023-08-25 16:01:43 -0500

    limit feed depth to 25

--- src/app/feed/page.tsx
+++ src/app/feed/page.tsx
@@ -2,9 +2,9 @@ import { sql } from '@vercel/postgres';
 
 async function getInteractions() {
     // TODO: try/catch block
-    const res = await sql`SELECT * FROM interactions ORDER BY timestamp DESC`;
+    const res = await sql`SELECT * FROM interactions ORDER BY timestamp DESC LIMIT 25`;
     const interactions = res.rows;
-    
+
     interactions.forEach((interaction) => {
         interaction.timestamp = interaction.timestamp.toString();
     });
@@ -14,7 +14,7 @@ async function getInteractions() {
 
 export default async function Feed() {
     const interactions = await getInteractions();
-    
+
     return (
         <>
             <h1>Interactions</h1>
