ca845a2935749c40b25ece46a80dd935390847aa
woojinv <oh.woojin95@gmail.com>
2023-08-25 15:23:47 -0500

    use collapse display for feed questions/answers

--- src/app/feed/page.tsx
+++ src/app/feed/page.tsx
@@ -1,25 +1,37 @@
 import { sql } from '@vercel/postgres';
 
 async function getInteractions() {
-    const res = await sql`SELECT * FROM interactions`;
-    const interactions = res.rows.sort((a, b) => b.timestamp - a.timestamp);
+    // TODO: try/catch block
+    const res = await sql`SELECT * FROM interactions ORDER BY timestamp DESC`;
+    const interactions = res.rows;
+    
+    interactions.forEach((interaction) => {
+        interaction.timestamp = interaction.timestamp.toString();
+    });
+
     return interactions;
 }
 
 export default async function Feed() {
     const interactions = await getInteractions();
+    
     return (
         <>
             <h1>Interactions</h1>
             {interactions.map((interaction) => (
-                <>
+                <div key={interaction.id} className="collapse bg-base-200">
+                    <input type="checkbox" />
+                    <div className="collapse-title text-xl font-medium">
+                        <p>Passage: {interaction.passage}</p>
+                        <p>Question: {interaction.question}</p>
+                        <p>Date: {interaction.timestamp}</p>
+                    </div>
+                    <div className="collapse-content">
+                        <p>Answer: {interaction.answer}</p>
+                    </div>
+
                     <br />
-                    <div>Passage: {interaction.passage}</div>
-                    <div>Question: {interaction.question}</div>
-                    <div>Answer: {interaction.answer}</div>
-                    <div>Date: {interaction.timestamp.toString()}</div>
-                    <br />
-                </>
+                </div>
             ))}
         </>
     );
